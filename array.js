709320abf46c8c629d3d16d3664743d17d6bdbd6
Chhoeurn kimla <chhoeurnkimla@gmail.com>
2024-02-16 21:28:25 +0700

    First commit

---
+++ array.js
@@ -0,0 +1,72 @@
+let arr = [12, 33, 44, 42];
+console.log(arr.length);
+
+// Array Object
+
+let products = [
+  {
+    title: "Mac 2024",
+    description: "New Mac form USA",
+    price: 2000,
+    thumbnail: `https://i.pinimg.com/474x/0e/c0/07/0ec00754c23735c5b923a589b3ddf4dd.jpg`,
+  },
+  {
+    title: "Mac 2023",
+    description: "New Mac form USA",
+    price: 900,
+    thumbnail: `https://i.pinimg.com/474x/79/fb/96/79fb967978e05c8a7b8fbf6972a3b191.jpg`,
+  },
+  {
+    title: "Mac 2022",
+    description: "New Mac form USA",
+    price: 3000,
+    thumbnail: `https://i.pinimg.com/474x/b0/c9/37/b0c937b6949dd7118fdeefa15e85a686.jpg`,
+  },
+  {
+    title: "Mac 2021",
+    description: "New Mac form USA",
+    price: 1500,
+    thumbnail: `https://i.pinimg.com/474x/31/71/0a/31710a071e75e2817898876cc755ccb2.jpg`,
+  },
+];
+
+let SECTION = document.querySelector("section");
+console.log(products[0].title);
+products.map((product) => {
+  SECTION.innerHTML += ` 
+  <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<a href="#">
+    <img class="p-8 rounded-t-lg" src="${product.thumbnail}" alt="product image" />
+</a>
+<div class="px-5 pb-5">
+    <a href="#">
+        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product.title} <br> ${product.description}</h5>
+    </a>
+    <div class="flex items-center mt-2.5 mb-5">
+        <div class="flex items-center space-x-1 rtl:space-x-reverse">
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+            <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+            </svg>
+        </div>
+        <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+    </div>
+    <div class="flex items-center justify-between">
+        <span class="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
+        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+    </div>
+</div>
+</div>
+`;
+});
